9b61c9f23b9f4bcf1bb65027474c028f724cbaac
Yulia Garifulina <hackwrenchh@gmail.com>
2016-07-12 16:57:51 +0300

    Add open and close functions for modal content

--- js/script.js
+++ js/script.js
@@ -24,3 +24,19 @@
     myMap.geoObjects.add(myPlacemark);
   }
 })();
+
+var link = document.querySelector(".open-modal");
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
+var name = popup.querySelector("[name=fullname]");
+
+link.addEventListener("click", function(event) {
+  event.PreventDefault();
+  popup.classList.add("modal-content-show");
+  name.focus();
+});
+
+close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+  });
